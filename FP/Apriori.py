5bf948ef3775743aea8493945ea3da0239f0fce0
KingJ <501315555@qq.com>
2019-03-24 02:04:29 +0800

    2019-3-24
    明天再说~

--- FP/Apriori.py
+++ FP/Apriori.py
@@ -17,7 +17,7 @@ def load_data():
 
 
 # 扫描并对数据库进行support_count
-def support_count(transactions):
+def support_count_c1(transactions):
     dic = dict.fromkeys(range(120), 0)
 
     for items in transactions:
@@ -36,6 +36,19 @@ def support_count(transactions):
     # write_into_excel(dic)
 
     return dic
+
+
+def support_count(transactions, ck):
+    ck_support = {}
+    for transaction in transactions:
+        for item in ck:
+            if item.issubset(transaction):
+                if item in ck_support:
+                    ck_support[item] += 1
+                else:
+                    ck_support[item] = 1
+
+    return ck_support
 
 
 # 将每个项的support_count写入Excel表格
@@ -58,32 +71,34 @@ def write_into_excel(dic):
 
 
 def Apriori_L1(ck, min_sup):
-
     '''
     :param ck: 候选项集
     :param min_sup: 最小支持度阈值
     :return: 返回频繁1项集
     '''
 
-    Lk = []
+    l1 = set()
     for item in ck.keys():
         if ck[item] > min_sup:
-            Lk.append(item)
+            frequent_item = frozenset(str(item))
+            l1.add(frequent_item)
 
-    return Lk
+    return l1
 
 
 def Apriori(ck_support, min_sup):
     '''
-    用于将精简后的ck进行统计得到
-    :param ck_support:
-    :param min_sup:
+    用于将精简后的ck进行统计得到ck_support
+    将ck_support与min_sup进行比较筛选出频繁集
+    :param ck_support: 经统计后ck中候选项的计数
+    :param min_sup: 最小支持度阈值
     :return: 返回频繁项集
     '''
-    lk = []
+    lk = set()
     for item in ck_support.keys():
         if ck_support[item] > min_sup:
-            lk.append(item)
+            frequent_item = frozenset(str(item))
+            lk.add(frequent_item)
 
     return lk
 
@@ -91,25 +106,28 @@ def Apriori(ck_support, min_sup):
 def Apriori_gen(lk_sub_1, k):
     '''
     连接步：将Lk-1与自身连接，当两个元素前k-2个项相同时，才进行来连接
-    目的是减少遍历次数，确保不产生重复
+    目的是减少遍历次数，确保不产生重复项
     :param lk_sub_1: 表示频繁k-1项集Lk-1，用于构造候选集Ck
     :param k: 当前候选集的项数
     :return: 生成ck候选项集
     '''
-    ck = []
+    ck = set()
     l1 = []
     l2 = []
     lk_len = len(lk_sub_1)
+    lk = list(lk_sub_1)
     for i in range(lk_len):
         for j in range(i+1, lk_len):
-            l1 = lk_sub_1[i][:k-2]
-            l2 = lk_sub_1[j][:k-2]
+            l1 = list(lk[i])
+            l2 = list(lk[j])
+            l1.sort()
+            l2.sort()
 
             # 如果l1和l2的前k-2项相同，则将
-            if l1 == l2:
-                ck_item = lk_sub_1[i] | lk_sub_1[j]
+            if l1[:k-2] == l2[:k-2]:
+                ck_item = lk[i] | lk[j]
                 if is_Apriori(ck_item, lk_sub_1):
-                    ck.append(ck_item)
+                    ck.add(frozenset(ck_item))
 
     for c in ck:
         if not is_Apriori(c, lk_sub_1):
@@ -136,19 +154,23 @@ def is_Apriori(ck, lk_sub_1):
 if __name__ == '__main__':
     min_sup = 1000
     L = [[]]
+    c = [[], []]
     c_support = [[]]
 
-    transaction = load_data()
-    c_support.append(support_count(transaction))
+    data = load_data()
+    c_support.append(support_count_c1(data))
     L.append(Apriori_L1(c_support[1], min_sup))
 
-    print(L)
+    # print(L)
     k = 2
 
-    c_support.append(Apriori())
+    while L[k-1]:
+        c.append(Apriori_gen(L[k - 1], k))
+        c_support.append(support_count(data, c[k]))
+        L.append(Apriori(c_support[k], min_sup))
+        k += 1
 
-    # while L[k-1]:
-    #     Ck = Apriori_gen(L[k-1], k)
-    #     k += 1
+    print(L)
+    print(c)
+    print(c_support)
 
-    #     L[k] = L[k-1]
